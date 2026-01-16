05130753ff72292ad6f764f8f7a8d834fda5d200
Qfactor <qfactor2013@gmail.com>
2026-01-16 16:13:39 +0900

    Update allowed origin in CORS configuration
    Update allowed origin in CORS configuration

--- src/main/java/com/example/devso/config/SecurityConfig.java
+++ src/main/java/com/example/devso/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:*", "https://renewed-jacquetta-testtestohtest-b687e626.koyeb.app"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:*", "https://devso.koyeb.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
